2a2c1b6b0670155eb1591465accb6cf39eb63cd7
Bulrock <maksim.volozhinskij@gmail.com>
2022-08-26 00:42:34 +0300

    feat: implement functions for getting cards from deck and updating number of stage cards; update active styles for chosen ancient card and difficulty buttons

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const state = {
     greenCardsStack: [],
     blueCardsStack: [],
     brownCardsStack: [],
+    stage1Cards: [],
+    stage2Cards: [],
+    stage3Cards: [],
+    firstStageNumbers: {},
+    secondStageNumbers: {},
+    thirdStageNumbers: {},
 };
 const ancientsContainer = document.querySelector('.ancients-container');
 const difficultyContainer = document.querySelector('.difficulty-container');
@@ -29,7 +35,9 @@ ancientsContainer.onclick = function (event) {
         return
     }
     state.ancientCard = ancientsData.find( card => card.id === ancientId);
-   
+    lastCard.style['background-image'] = 'unset';
+    ancientsContainer.querySelectorAll('.ancient-card').forEach(card => card.classList.remove("active"));
+    target.classList.add("active");
 };
 
 difficultyContainer.onclick = function (event) {
@@ -43,6 +51,10 @@ difficultyContainer.onclick = function (event) {
     showStageNumbers (secondStageContainer, state.ancientCard.secondStage);
     showStageNumbers (thirdStageContainer, state.ancientCard.thirdStage);
     getCardsForState ()
+    lastCard.style['background-image'] = 'unset';
+
+    difficultyContainer.querySelectorAll('.difficulty').forEach(button => button.classList.remove("active"));
+    target.classList.add("active");
 };
 
 function showStageNumbers (stageContainer, stage){
@@ -54,17 +66,39 @@ function showStageNumbers (stageContainer, stage){
 stack.addEventListener('click', takeCard);
 
 function takeCard(){
-    let number = getRandomNum(2);
-    if()
+    let chosenCard;
+    let stageNumbersToUpdate;
+    let stageContainer;
+    if(state.stage1Cards.length > 0){
+        chosenCard = state.stage1Cards.pop()
+        stageNumbersToUpdate = state.firstStageNumbers;
+        stageContainer = firstStageContainer;
+    } else if(state.stage2Cards.length > 0){
+        chosenCard = state.stage2Cards.pop()
+        stageNumbersToUpdate = state.secondStageNumbers;
+        stageContainer = secondStageContainer;
+    } else if(state.stage3Cards.length > 0){
+        chosenCard = state.stage3Cards.pop()
+        stageNumbersToUpdate = state.thirdStageNumbers;
+        stageContainer = thirdStageContainer;
+    } else {
+        return
+    }
 
-    lastCard.style['background-image'] = 'url(./assets/MythicCards/blue/blue1.png)'
-}
+    if(chosenCard.color === 'green'){
+        stageNumbersToUpdate.greenCards -= 1;
+    } else if(chosenCard.color === 'brown'){
+        stageNumbersToUpdate.brownCards -= 1;
+    } else if(chosenCard.color === 'blue'){
+        stageNumbersToUpdate.blueCards -= 1;
+    }
+    showStageNumbers (stageContainer, stageNumbersToUpdate)
 
-function getRandomNum(max) {
-    let result = 0;
-    let min = Math.ceil(1);
-    result = Math.floor(Math.random() * (max - min + 1)) + min;
-    return result
+    lastCard.style['background-image'] = `url(./assets/MythicCards/${chosenCard.color}/${chosenCard.id}.png)`
+
+    if(state.stage1Cards.length < 1 && state.stage2Cards.length < 1 && state.stage3Cards < 1){
+        stack.style.visibility = "hidden";
+    }
 }
 
 function shuffle(array) {
@@ -83,6 +117,29 @@ function getCardsForState (){
     state.greenCardsStack = getCardsForStages ('greenCards');
     state.blueCardsStack = getCardsForStages ('blueCards');
     state.brownCardsStack = getCardsForStages ('brownCards');
+
+    state.stage1Cards = [];
+    state.stage1Cards = state.stage1Cards.concat(state.greenCardsStack.splice(0, state.ancientCard.firstStage.greenCards));
+    state.stage1Cards = state.stage1Cards.concat(state.brownCardsStack.splice(0, state.ancientCard.firstStage.brownCards));
+    state.stage1Cards = state.stage1Cards.concat(state.blueCardsStack.splice(0, state.ancientCard.firstStage.blueCards));
+    state.stage1Cards = shuffle(state.stage1Cards);
+
+    state.stage2Cards = [];
+    state.stage2Cards = state.stage2Cards.concat(state.greenCardsStack.splice(0, state.ancientCard.secondStage.greenCards));
+    state.stage2Cards = state.stage2Cards.concat(state.brownCardsStack.splice(0, state.ancientCard.secondStage.brownCards));
+    state.stage2Cards = state.stage2Cards.concat(state.blueCardsStack.splice(0, state.ancientCard.secondStage.blueCards));
+    state.stage2Cards = shuffle(state.stage2Cards);
+
+    state.stage3Cards = [];
+    state.stage3Cards = state.stage3Cards.concat(state.greenCardsStack.splice(0, state.ancientCard.thirdStage.greenCards));
+    state.stage3Cards = state.stage3Cards.concat(state.brownCardsStack.splice(0, state.ancientCard.thirdStage.brownCards));
+    state.stage3Cards = state.stage3Cards.concat(state.blueCardsStack.splice(0, state.ancientCard.thirdStage.blueCards));
+    state.stage3Cards = shuffle(state.stage3Cards);
+
+    state.firstStageNumbers = {...state.ancientCard.firstStage};
+    state.secondStageNumbers = {...state.ancientCard.secondStage};
+    state.thirdStageNumbers = {...state.ancientCard.thirdStage};
+    stack.style.visibility = "unset";
 }
 
 function getCardsForStages (colorOfCards){
